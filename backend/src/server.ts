896e030c231496897c6e5f6cbaf7e5cff32199ae
Bruno Vettori <koikke21@gmail.com>
2020-10-16 17:33:11 -0300

    .

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,41 @@
 import Express from 'express'
+import { getRepository } from 'typeorm'
+import Orphanages from './models/Orphanage'
 
-const app = Express()
+import './database/connection'
+import Orphanage from './models/Orphanage'
 
-app.get('/users', (request, response) => {
-    return response.json({message: 'Hello World'})
+const app = Express();
+
+app.use(Express.json());
+
+app.post('/orphanages', async (request, response) => {
+    console.log('body',request.body);
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    } = request.body
+
+    const orphanagesRepository = getRepository(Orphanages);
+
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    })
+
+    await orphanagesRepository.save(orphanage)
+
+    return response.status(201).json(orphanage)
 })
 
 app.listen(3333)
